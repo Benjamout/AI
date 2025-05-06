8d51a0d9a42db3a2a90aef7a1feb90d415509096
Connor-Mawer <connormawer@gmail.com>
2025-05-06 14:33:10 +1000

    Updated setup.py corrcetly

--- simple-carpark-env/setup.py
+++ simple-carpark-env/setup.py
@@ -1,12 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="simple_carpark",
     version='0.0.1',
-    install_requires=['gym',
-                      'pybullet',
-                      'numpy',
-                      'matplotlib',
-                      'torch'],
-    package_data={'simple_driving': ['resources/*.urdf']}
+    install_requires=[
+        'gym',
+        'pybullet',
+        'numpy',
+        'matplotlib',
+        'torch'
+    ],
+    packages=find_packages(include=["simple_carpark", "simple_carpark.*"]),
+    package_data={'simple_carpark': ['resources/*.urdf']}
 )
